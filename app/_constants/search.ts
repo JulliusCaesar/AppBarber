56f5d723879db24cc0b20d0f0f5a2dbd5f302f0d
Julio Cesar <julliusrock138@gmail.com>
2024-09-29 16:12:39 -0300

    feat: delete some services

--- app/_constants/search.ts
+++ app/_constants/search.ts
@@ -16,16 +16,16 @@ export const quickSearchOptions: QuickSearchOption[] = [
     imageUrl: "/acabamento.svg",
     title: "Acabamento",
   },
-  {
-    imageUrl: "/massagem.svg",
-    title: "Massagem",
-  },
-  {
-    imageUrl: "/sobrancelha.svg",
-    title: "Sobrancelha",
-  },
-  {
-    imageUrl: "/hidratacao.svg",
-    title: "Hidratação",
-  },
+  // {
+  //   imageUrl: "/massagem.svg",
+  //   title: "Massagem",
+  // },
+  // {
+  //   imageUrl: "/sobrancelha.svg",
+  //   title: "Sobrancelha",
+  // },
+  // {
+  //   imageUrl: "/hidratacao.svg",
+  //   title: "Hidratação",
+  // },
 ]
